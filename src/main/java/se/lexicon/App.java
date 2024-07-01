2cc69249b35fad1d3acf62540ef7b598514bbe40
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-07-01 21:51:34 +0200

    Modified

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,18 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+
+    public static void main( String[] args ) {
+
+        System.out.println("Select Operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Exit");
+        System.out.println("Enter your choice: ");
+
     }
+
 }
